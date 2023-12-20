04b61059daa2e4d06af707350d34574a973936e0
iamshobh <iamshobhraj@gmail.com>
2023-12-20 16:38:35 +0530

    fixed mobile view height

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <div className={'flex flex-col md:flex-row h-screen bg-white'}>
+      <div className={'flex flex-col md:flex-row md:h-screen h-svh bg-white'}>
         <Card culor='rgba(142, 0, 255, 0.4)' img_src='IEEE_Whitelogo.png' text='IEEE'/>
         <Card culor='rgba(255, 169, 0, 0.4)' img_src='robotics.png' text='ROBOTICS'/>
         <Card culor='rgba(0, 56, 255, 0.4)' img_src='Asce bg removed.png' text='ASCE'/>
